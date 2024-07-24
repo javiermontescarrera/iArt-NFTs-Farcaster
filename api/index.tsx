5a53a0942aefd6e3490a5b74ec3f97d6693d8fd2
Javier Montes <javier.montes.carrera@gmail.com>
2024-07-24 12:20:11 -0500

    First commit

---
+++ api/index.tsx
@@ -0,0 +1,209 @@
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+import { serveStatic } from 'frog/serve-static'
+import { colors } from 'frog/ui'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/vercel'
+
+// Uncomment to use Edge Runtime.
+// export const config = {
+//   runtime: 'edge',
+// }
+
+export const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+  title: 'Frog Frame',
+})
+
+// app.frame('/', (c) => {
+//   const { buttonValue, inputText, status } = c
+//   const fruit = inputText || buttonValue
+//   return c.res({
+//     image: (
+//       <div
+//         style={{
+//           alignItems: 'center',
+//           background:
+//             status === 'response'
+//               ? 'linear-gradient(to right, #432889, #17101F)'
+//               : 'black',
+//           backgroundSize: '100% 100%',
+//           display: 'flex',
+//           flexDirection: 'column',
+//           flexWrap: 'nowrap',
+//           height: '100%',
+//           justifyContent: 'center',
+//           textAlign: 'center',
+//           width: '100%',
+//         }}
+//       >
+//         <div
+//           style={{
+//             color: 'white',
+//             fontSize: 60,
+//             fontStyle: 'normal',
+//             letterSpacing: '-0.025em',
+//             lineHeight: 1.4,
+//             marginTop: 30,
+//             padding: '0 120px',
+//             whiteSpace: 'pre-wrap',
+//           }}
+//         >
+//           {status === 'response'
+//             ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
+//             : 'Welcome!'}
+//         </div>
+//       </div>
+//     ),
+//     intents: [
+//       <TextInput placeholder="Enter custom fruit..." />,
+//       <Button value="apples">Apples</Button>,
+//       <Button value="oranges">Oranges</Button>,
+//       <Button value="bananas">Bananas</Button>,
+//       status === 'response' && <Button.Reset>Reset</Button.Reset>,
+//     ],
+//   })
+// })
+
+app.frame('/', (c) => {
+  const { buttonValue, inputText, status } = c
+  const fruit = inputText || buttonValue
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background: 'linear-gradient(to right, #432889, #17101F)',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            color: 'white',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          <p 
+            style={{ 
+              fontSize: 49,
+              fontStyle: 'bold',
+              textAlign: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            Bienvenido/a a iArt NFTs!
+          </p>
+          <p 
+            style={{ 
+              fontSize: 35,
+              fontStyle: 'normal',
+              color: 'yellow',
+            }}
+          >
+            Aqui podras crear tus NFTs con imagenes generadas por IA
+          </p>
+        </div>
+      </div>
+    ),
+    intents: [
+      // <Button.Link href="https://frutero.club">Frutero</Button.Link>,
+      <Button action="/step-1">Empezar!</Button>,
+    ],
+  })
+})
+
+app.frame('/step-1', (c) => {
+  const { buttonValue, inputText, status } = c
+  const userInput = inputText || buttonValue
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          background: 'linear-gradient(to right, #432889, #17101F)',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            fontSize: 35,
+            color: 'yellow',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          <p 
+            style={{ 
+              fontSize: 56,
+              color: 'white',
+              fontStyle: 'bold',
+              textAlign: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            Paso 1
+          </p>
+          <p>
+            Escribe un estilo de arte y una descripcion.
+          </p>
+          <p 
+            style={{
+              justifyContent: 'center',
+              textAlign: 'center',
+            }}
+          >
+            o
+          </p>
+          <p>
+            Presiona el boton para generar una descripcion.
+          </p>
+        </div>
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="Ingresa estilo de arte y descripción..." />,
+      <Button action='/step-2'>Generar descripcion</Button>,
+      <Button action='/step-2'>Cotinuar</Button>,
+      <Button.Reset>Reiniciar</Button.Reset>,
+    ],
+  })
+})
+
+
+
+// @ts-ignore
+const isEdgeFunction = typeof EdgeFunction !== 'undefined'
+const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development'
+devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
